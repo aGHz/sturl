5e80f92d739c18fd31819056d5020f8a78df03e7
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2014-05-14 16:23:25 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,28 @@
+sturl
+=====
+
+*URLs for humans*
+
+
+Features
+--------
+
+
+Overview
+--------
+
+Install
+-------
+
+You can install sturl from PyPI with `pip <http://www.pip-installer.org/>`_:
+
+.. code-block:: sh
+
+    $ pip install sturl
+
+or from `GitHub <https://github.com/aGHz/sturl>`_ with git:
+
+.. code-block:: sh
+
+    $ git clone https://github.com/aGHz/sturl.git
+    $ cd sturl && python setup.py install
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.1.0'
+DESC = open('README.rst').read()
+
+setup(
+    name = 'sturl',
+    packages = ['sturl'],
+    version = VERSION,
+    description = 'URLs for humans',
+    long_description = DESC,
+    author = 'Adrian Ghizaru',
+    author_email = 'adrian.ghizaru@gmail.com',
+    url = 'https://github.com/aGHz/sturl',
+    license = 'MIT',
+
+    install_requires = [
+    ],
+
+    zip_safe = False,
+    include_package_data = True,
+    package_data = {'': ['LICENSE', 'README.rst']},
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+    keywords = ['url'],
+
+    download_url = 'https://github.com/aGHz/sturl/tarball/{v}'.format(v=VERSION)
+)
